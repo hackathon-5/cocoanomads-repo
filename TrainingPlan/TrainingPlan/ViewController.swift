ee56f643ac1fee62dfe84719e71be5cf6e3d5c54
abrewing <alondob@gmail.com>
2015-08-28 21:29:51 -0400

    create initial app project

---
+++ TrainingPlan/TrainingPlan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingPlan
+//
+//  Created by Alondo  on 8/28/15.
+//  Copyright (c) 2015 CocoaNomads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
